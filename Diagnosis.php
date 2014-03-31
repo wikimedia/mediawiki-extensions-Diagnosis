66298345b7a93db95d181f5db4465eb65b246f1c
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-31 09:50:07 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: I1233e6eb7511474cb2dc2a28a04d91a6d21b7d3b

--- Diagnosis.php
+++ Diagnosis.php
@@ -7,23 +7,24 @@ require_once( "\$IP/extensions/Diagnosis/Diagnosis.php" );
 EOT;
 		exit( 1 );
 }
- 
+
 $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'Diagnosis',
 	'author' => '[http://www.dasch-tour.de DaSch]',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Diagnosis',
 	'descriptionmsg' => 'diagnosis-desc',
-	'version' => '0.0.1',
+	'version' => '0.1.0',
 );
- 
+
 $dir = dirname(__FILE__) . '/';
 
 $wgAvailableRights[] = 'diagnosis-access';
 $wgGroupPermissions['sysop']['diagnosis-access'] = true;
- 
+
 $wgAutoloadClasses['SpecialDiagnosis'] = $dir . 'SpecialDiagnosis.php'; # Location of the SpecialMyExtension class (Tell MediaWiki to load this file)
-$wgExtensionMessagesFiles['Diagnosis'] = $dir . 'Diagnosis.i18n.php'; # Location of a messages file (Tell MediaWiki to load this file)
+$wgMessagesDirs['Diagnosis'] = __DIR__ . '/i18n';
+$wgExtensionMessagesFiles['Diagnosis'] = $dir . 'Diagnosis.i18n.php';
 $wgExtensionMessagesFiles['SpecialDiagnosis'] = $dir . 'Diagnosis.i18n.alias.php';
 $wgSpecialPages['Diagnosis'] = 'SpecialDiagnosis'; # Tell MediaWiki about the new special page and its class name
 $wgSpecialPageGroups['Diagnosis'] = 'other';
