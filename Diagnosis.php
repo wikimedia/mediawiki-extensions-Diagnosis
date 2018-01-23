961f40e67198f4c13a533f2c509f549d96a63460
Jayprakash12345 <0freerunning@gmail.com>
2018-01-23 22:40:16 +0530

    Convert Diagnosis to use extension registration
    Bug: T185585
    Change-Id: Ifcf47ecf48828154920d456e5e7f08ffe6d6dcec

--- Diagnosis.php
+++ Diagnosis.php
@@ -1,28 +1,15 @@
 <?php
-# Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
-if (!defined('MEDIAWIKI')) {
-		echo <<<EOT
-To install my extension, put the following line in LocalSettings.php:
-require_once( "\$IP/extensions/Diagnosis/Diagnosis.php" );
-EOT;
-		exit( 1 );
+
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'Diagnosis' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Diagnosis'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the Diagnosis extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the Diagnosis extension requires MediaWiki 1.29+' );
 }
-
-$wgExtensionCredits['specialpage'][] = array(
-	'path' => __FILE__,
-	'name' => 'Diagnosis',
-	'author' => '[http://www.dasch-tour.de DaSch]',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:Diagnosis',
-	'descriptionmsg' => 'diagnosis-desc',
-	'version' => '0.1.0',
-);
-
-$dir = dirname(__FILE__) . '/';
-
-$wgAvailableRights[] = 'diagnosis-access';
-$wgGroupPermissions['sysop']['diagnosis-access'] = true;
-
-$wgAutoloadClasses['SpecialDiagnosis'] = $dir . 'SpecialDiagnosis.php'; # Location of the SpecialMyExtension class (Tell MediaWiki to load this file)
-$wgMessagesDirs['Diagnosis'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['SpecialDiagnosis'] = $dir . 'Diagnosis.i18n.alias.php';
-$wgSpecialPages['Diagnosis'] = 'SpecialDiagnosis'; # Tell MediaWiki about the new special page and its class name
